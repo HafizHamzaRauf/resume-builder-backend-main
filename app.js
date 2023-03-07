8e03a2fd31fdd4ee146a7415a0c60dbe8eec9203
hamza <hamazrauf@gmail.com>
2023-03-07 21:12:20 +0500

    update the order

--- app.js
+++ app.js
@@ -14,11 +14,12 @@ app.use(cors());
 app.use(bodyParser.json());
 
 //**************    MAIN MIDDLEWARES **********************/
+
+app.use("/auth", authRoutes);
 app.use("/", (req, res, next) => {
   console.log("jobify is running");
   return res.status(200).json({ message: "jobify is running" });
 });
-app.use("/auth", authRoutes);
 // Listen on enviroment variable PORT or 3000
 const port = process.env.PORT;
 app.listen(port, () => {
