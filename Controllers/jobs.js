656b03053eef1c853aff2e30ab5534d68047a183
hamza <hamazrauf@gmail.com>
2023-03-09 23:35:59 +0500

    added the endpoint to delete the job

--- Controllers/jobs.js
+++ Controllers/jobs.js
@@ -26,3 +26,42 @@ exports.addJob = async (req, res, next) => {
     return res.status(500).json({ ok: false, message: "something went wrong" });
   }
 };
+
+exports.delete = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    // verify  token
+    const user = jwt.verify(token, process.env.PRIVATE_KEY);
+    if (!user) {
+      return res.status(400).json({ ok: false, message: "invalid token" });
+    }
+
+    const { email, userId } = user;
+
+    const loadedUser = await User.findOne({ email, _id: userId });
+    if (!loadedUser) {
+      return res.status(400).json({ ok: false, message: "User not found" });
+    }
+
+    const { jobId } = req.body;
+    const isFound = loadedUser.jobs.find(
+      (item) => item._id.toString() === jobId
+    );
+    if (!isFound) {
+      return res.status(400).json({ ok: false, message: "job not found " });
+    }
+
+    const newJobs = loadedUser.jobs.filter((item) => {
+      return item._id.toString() !== jobId;
+    });
+
+    loadedUser.jobs = newJobs;
+    await loadedUser.save();
+    return res
+      .status(200)
+      .json({ ok: true, message: "job deleted Successfully" });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(400).json({ ok: false, message: "something went wrong" });
+  }
+};
--- routes/jobs.js
+++ routes/jobs.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const JobController = require("../Controllers/jobs");
 router.post("/add", JobController.addJob);
-
+router.post("/delete", JobController.delete);
 module.exports = router;
